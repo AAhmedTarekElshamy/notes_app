9e670fedb5e5f1045a41aaebd7672e2084ddaf7b
Ahmed Tarek <ahmedtarik20188@gmail.com>
2024-07-04 04:51:25 +0300

    add BorderRadius to showModalBottomSheet()

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/custom_notes_item.dart';
 import 'package:notes_app/views/widgets/custom_search_icon.dart';
 import 'package:notes_app/views/widgets/notes_body.dart';
@@ -21,9 +22,15 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.grey.withOpacity(0.3),
       floatingActionButton:FloatingActionButton(
         onPressed: (){
-          showModalBottomSheet(context: context, builder:(context){
-            return Container();
-          } )
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context, builder:(context){
+            return AddNoteBottomSheet(
+
+            );
+          } );
         },
         child:const Icon(Icons.add),
       ),
