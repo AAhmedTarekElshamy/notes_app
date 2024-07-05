1ec9382f7fe86becd1d0ba3c7e0fc33f7158e26e
Ahmed Tarek <ahmedtarik20188@gmail.com>
2024-07-06 02:01:02 +0300

    init hive & open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/home_page.dart';
+import 'package:notes_app/views/widgets/constants.dart';
 
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
--- lib/views/widgets/constants.dart
+++ lib/views/widgets/constants.dart
@@ -1,3 +1,4 @@
  import 'package:flutter/material.dart';
 
 const  KPrimaryColor = Color(0xff62FDC7);
+const kNotesBox = 'notes_box';
